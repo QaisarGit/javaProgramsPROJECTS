15fe31f5e7b9f486e56b19f035bb90505e0d3825
QaisarGit <qaisar.fareed@gmail.com>
2026-03-13 02:30:33 +0530

    git java 106

--- src/test/java/StringManipulation/RemoveSpaces.java
+++ src/test/java/StringManipulation/RemoveSpaces.java
@@ -1,5 +1,16 @@
 package StringManipulation;
 
+import org.testng.annotations.Test;
+
 public class RemoveSpaces {
 
+	@Test(description="remove spaces ")
+	public void rmvSpaces() {
+		
+		String text = "   Java  is  fun   ";
+        // The regular expression "\\s+" matches one or more whitespace characters
+        String noSpaces = text.replaceAll("\\s+", "");
+        System.out.println("Original string: \"" + text + "\"");
+        System.out.println("String without spaces: \"" + noSpaces + "\"");
+	}
 }
