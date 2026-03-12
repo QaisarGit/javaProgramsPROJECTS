ede6723d99c96c60e4283fd4a8868c87b764327e
QaisarGit <qaisar.fareed@gmail.com>
2026-03-13 02:12:35 +0530

    git java 104

--- src/test/java/StringManipulation/Reverse.java
+++ src/test/java/StringManipulation/Reverse.java
@@ -1,5 +1,28 @@
 package StringManipulation;
 
-public class Reverse {
+import org.testng.annotations.Test;
 
-}
+public class Reverse {
+	
+	@Test(description="string builder")
+	public void ReversedSB() {
+		
+		String str= "I am java coding language fun type guy ";
+		StringBuilder sb = new StringBuilder(str);
+		StringBuilder rev= sb.reverse();
+		System.out.println("reversed string is \n\t"+rev);
+	}
+
+	@Test(description="using loop")
+	public void ReversedLoop() {
+		
+		String str= "I am java coding language fun type guy ";
+		String rev="";
+		for(int i=str.length()-1;i>=0;i--) {
+			rev=rev+str.charAt(i);
+		}
+		System.out.println("reveresed string is ====  "+rev);
+
+		
+	}
+} 
