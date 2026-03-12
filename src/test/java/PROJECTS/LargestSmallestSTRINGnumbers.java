db00418d306cfe799d8502082a55472cdcca733a
QaisarGit <qaisar.fareed@gmail.com>
2026-03-12 23:14:54 +0530

    git push java 101

--- src/test/java/PROJECTS/LargestSmallestSTRINGnumbers.java
+++ src/test/java/PROJECTS/LargestSmallestSTRINGnumbers.java
@@ -4,26 +4,37 @@ import org.testng.annotations.Test;
 
 public class LargestSmallestSTRINGnumbers {
 
-	@Test
-	public void largestWord() {
-		
-		String  str =" i ma wring a program in java programming language ";
-		String words[]= str.trim().split("\s+");
-		String less=words[0];
-		String larger="";
-		for(int i =1;i<str.length()-1;i++) {
-		
-		if(words.length>larger.length()) {
-			larger=words[i];
-			
-			
+	@Test(description="smallest", priority=1)
+	public void smallest() {
+
+		String str = " immmm mammm wring asss program in java programming language ";
+		String word[] = str.trim().split("\\s+");
+		String small = word[0];
+		for (int i = 0; i < word.length; i++) {
+			if (word[i].length() < small.length()) {
+				small = word[i];
+			}
 		}
-			
-			
-		}
-		
-		
-		
-		System.out.println(" largest word  >>>>>>>>> <<<<<<\n" +larger);
+		System.out.println("smallest word is ---------- \n\t"+small);
 	}
+
+	@Test(description = "largest word", priority=2)
+	public void largest() {
+		String str = " immmm mam wring asss program in java programming language ";
+		String word[]=str.trim().split("\\s+");
+		String large="";
+		//String large=word[0];
+		for(int i =1;i<word.length;i++) {
+			if(word[i].length()>large.length()) {
+				large=word[i];
+				
+				
+			}
+			
+			
+		}
+		System.out.println("largestes word ============  \n \t"+large);
+		
+	}
+
 }
