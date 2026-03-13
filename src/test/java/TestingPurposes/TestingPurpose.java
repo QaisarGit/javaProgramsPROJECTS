bd61554b50dd61b1c4b181abec3d12c80a57e99a
QaisarGit <qaisar.fareed@gmail.com>
2026-03-13 23:12:57 +0530

    git new branch 100

--- src/test/java/TestingPurposes/TestingPurpose.java
+++ src/test/java/TestingPurposes/TestingPurpose.java
@@ -1,7 +1,22 @@
 package TestingPurposes;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class TestingPurpose {
 	
-	
+	@Test
+	public void webServe() throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-chrome-driver/4.41.0");
+		Thread.sleep(1000);
+		driver.quit();
+		System.out.println("success===================");
+
+		
+	}
 
 }
